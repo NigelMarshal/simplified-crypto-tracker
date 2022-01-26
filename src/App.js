fba6b026dfb4605a4c79165207b8a16ae557bde7
NigelMarshal <nigelmarshal@gmail.com>
2022-01-26 21:08:10 +0400

    feat(App): Set up API call with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=AED&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error, "Error"));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>React</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search</h1>
+        <form action="">
+          <input type="text" placeholder="Search" className="coin-input" />
+        </form>
+      </div>
     </div>
   );
 }
